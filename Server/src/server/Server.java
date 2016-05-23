821437da9da0f45cdf00d3f51a92e0a512f6f5b8
RafaelMonteiro95 <rafaelmonteiro95@gmail.com>
2016-05-23 09:17:03 -0300

    update final, servidor funcionando no meu pc

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -7,7 +7,7 @@ import java.util.*;
 import bozo.*;
 
 /**
- *
+ * Classe que representa um servidor
  * @author Lucas Alexandre Soares - 9293265
  * @author Rafael Augusto Monteiro - 9293095
  */
@@ -19,19 +19,18 @@ public class Server {
 	/*TreeMap de jogadores
 	Armazena o id e a pontuacao de um jogador*/
 	private SortedMap<String,Player> players;
-	
-	
-	
-	public Server(){
+        
+    /**
+     * Construtor padrao da classe
+     */
+    public Server(){
 		this.flag = true;
 		this.players = Collections.synchronizedSortedMap(new TreeMap());
 		ss = null;
 	}
-	
-	/**
-	 * setter para a flag que diz se o servidor deve estar ligado ou nao
-	 */
-	public synchronized void setFlag(){
+
+	// setter para a flag que diz se o servidor deve estar ligado ou nao
+	private synchronized void setFlag(){
 		this.flag = false;
 	}
 	
@@ -41,36 +40,42 @@ public class Server {
 	 * @throws IOException 
 	 */
 	public void startServer(int port) throws IOException{
-		//ouvindo a porta
-		ss = new ServerSocket(port);
-		System.out.println("Conectado a porta " + port);
-		//crio uma thread para ouvir inputs da stdin
-		InputThread inThread = new InputThread(this);
-		inThread.start();
-		
-		//crio threads enquanto a inThread nao desligar a flag
-		while(this.flag){
-			
-			//crio um socket novo
-			Socket client = null;
-			//espero ateh alguem se conectar ao ServerSocket
-			try {
-				client = ss.accept();
-			} catch(Exception e){
-				System.out.println("Connection error: " + e);
-				return;
-			}
 
-			//disparo uma thread do servidor (que joga bozo com o cliente)
-			ServerThread st = new ServerThread(client, this);
-			st.start();
-		}
-		
-		if(!this.flag){
-			System.out.println("Top Player: " + this.players.get(this.players.lastKey()).getId());
-			System.out.println("Score: " + this.players.get(this.players.lastKey()).getScore());
-			System.out.println("Play count: " + this.players.get(this.players.lastKey()).getPC());
-		}
+            int nclients = 0;
+            //ouvindo a porta
+            ss = new ServerSocket(port);
+            System.out.println("Conectado a porta " + port);
+            //crio uma thread para ouvir inputs da stdin
+            InputThread inThread = new InputThread(this);
+            inThread.start();
+
+            //crio threads enquanto a inThread nao desligar a flag
+            while(this.flag){
+
+                //crio um socket novo
+                Socket client = null;
+                //espero ateh alguem se conectar ao ServerSocket
+                try {
+                    client = ss.accept();
+                    nclients++;
+                } catch(Exception e){
+                    System.out.println("Connection error: " + e);
+                    return;
+                }
+
+                //disparo uma thread do servidor (que joga bozo com o cliente)
+                ServerThread st = new ServerThread(client, this);
+                st.start();
+            }
+                
+            try {
+                Thread.sleep(1000l);
+            } catch (InterruptedException ex) {}
+            
+            System.out.println("Top Player: " + this.players.get(this.players.lastKey()).getId());
+            System.out.println("Score: " + this.players.get(this.players.lastKey()).getScore());
+            System.out.println("Play count: " + this.players.get(this.players.lastKey()).getPC());
+            System.out.println("Average score: " + (this.players.get(this.players.lastKey()).getScore()/this.players.get(this.players.lastKey()).getPC()));
 	}
 	
 	/**
@@ -92,122 +97,114 @@ public class Server {
 		
 		//construtor do thread
 		private ServerThread(Socket skt, Server s){
-			this.skt = skt;
-			this.s = s;
-			this.id = null;
-			this.rd = new RolaDados(5);
-			this.p = new Placar();
-			
-			//abrindo streams para se comunicar com o cliente
-			try{
-				this.serverIn = new Scanner(skt.getInputStream());
-				this.serverOut = new PrintStream(skt.getOutputStream());
-			} catch(Exception e){}
-			
+                    this.skt = skt;
+                    this.s = s;
+                    this.id = null;
+                    this.rd = new RolaDados(5);
+                    this.p = new Placar();
+
+                    //abrindo streams para se comunicar com o cliente
+                    try{
+                            this.serverIn = new Scanner(skt.getInputStream());
+                            this.serverOut = new PrintStream(skt.getOutputStream());
+                    } catch(Exception e){}
 		}
 		
 		@Override
 		public void run(){
-			
-			String msg = null;
-			String response = null;
-			
-			/*try-catch para evitar nullpointerexception caso algo seja finalizado
-			no meio da execucao de uma thread
-			achei melhor q esperar as threads acabarem
-			*/
-			try{
-				do {
-					msg = serverIn.nextLine();
-					// DEBUG
-					System.out.println("Client '" + this.id + "' msg: " + msg);
 
-					String[] args = msg.split(" ");
-					//decodifico a mensagem lida
-					if(args.length == 1) { //F e R<n>
-						if (args[0].equals("F")) { //F
-							
-							//finaliza partida
-							response = "\nFim de jogo\nPontuacao: " + p.getScore();
-							serverOut.println(response);
-							// DEBUG
-							System.out.println("SENDING @" + this.id + ": " + response);
+                    String msg = null;
+                    String response = null;
 
-							//gero um registro Player novo e insiro na lista de jogadores
-							Player plr = null;
-							if(s.players.get(this.id) == null) {
-								//insiro um novo player
-								plr = new Player(this.id, p.getScore());
-								s.players.put(this.id, plr);
-							} else {
-								//atualizo o score do player existente 
-								s.players.get(this.id).addScore(p.getScore());
-								s.players.get(this.id).incPC();
-							}
-							//finalizo a comunicacao com o cliente
-							flag = false;
-							endComm();
-							break;
-						} else { //R<n>
-							//chama a proxima rodada
-							response = Arrays.toString(rd.rolar());
-							serverOut.println(response);
-							// DEBUG
-							System.out.println("SENDING @" + this.id + ": " + response);
-						}
+                    /*try-catch para evitar nullpointerexception caso algo seja finalizado
+                    no meio da execucao de uma thread
+                    achei melhor q esperar as threads acabarem
+                    */
+                    try{
+                        do {
+                            msg = serverIn.nextLine();
+                            if(this.id == null){
+                                System.out.println("New client @ "+ skt.getInetAddress() + " says " + msg);   
+                            }
 
-					} else if(args.length == 2){ //I <id> e P<n> <pos>
+                            String[] args = msg.split(" ");
+                            //decodifico a mensagem lida
+                            if(args.length == 1) { //F e R<n>
+                                if (args[0].equals("F")) { //F
 
-						if(args[0].equals("I")){ //I <id>
+                                    //finaliza partida
+                                    response = "\nFim de jogo\nPontuacao: " + p.getScore();
+                                    serverOut.println(response);
 
-							//Altero o id da thread
-							id = args[1];
-							response = "Bem vindo!";
-							serverOut.println(response);
-							// DEBUG
-							System.out.println("SENDING @" + this.id + ": " + response);
+                                    //gero um registro Player novo e insiro na lista de jogadores
+                                    Player plr = null;
+                                    if(s.players.get(this.id) == null) {
+                                        //insiro um novo player
+                                        plr = new Player(this.id, p.getScore());
+                                        s.players.put(this.id, plr);
+                                    } else {
+                                        //atualizo o score do player existente 
+                                        s.players.get(this.id).addScore(p.getScore());
+                                        s.players.get(this.id).incPC();
+                                    }
+                                    //finalizo a comunicacao com o cliente
+                                    System.out.println(this.id + " scored " + p.getScore()); 
+                                    endComm();
+                                    break;
+                                } else { //R<n>
+                                    //chama a proxima rodada
+                                    rd.rolar();
+                                    response = rd.getStringDados();
+                                    serverOut.println(response);
+                                }
 
-						} else { //P<n> <pos>
-							//Coloco os pontos no placar
-							p.add(Integer.parseInt(args[1]), rd.getDados());
-							response = "" + p.getScore();
-							serverOut.println(response);
-							// DEBUG
-							System.out.println("SENDING @" + this.id + ": " + response);
+                        } else if(args.length == 2){ //I <id> e P<n> <pos>
 
-						}
+                            if(args[0].equals("I")){ //I <id>
 
-					} else { //T <d1> <d2> <d3> <d4> <d5>
-						//vetor com os dados a serem rolados
-					
-						boolean[] quais = new boolean[5];
-					
-						//preencho um vetor com os dados a serem rolados
-						for(int i = 1; i < 6; i++){
-							quais[i-1] = args[i].equals("1");
-						}   //rolo os dados
-						
-						response = Arrays.toString(rd.rolar(quais));
-						serverOut.println(response);
-						// DEBUG
-						System.out.println("SENDING @" + this.id + ": " + response);
-					}
+                                //Altero o id da thread
+                                id = args[1];
+                                response = "Bem vindo!";
+                                serverOut.println(response);
 
-				} while(serverIn.hasNextLine());
-			}catch(Exception e){}
+                            } else { //P<n> <pos>
+                                //Coloco os pontos no placar
+                                p.add(Integer.parseInt(args[1]), rd.getDados());
+                                response = "" + p.getScore();
+                                serverOut.println(response);
+                            }
+
+                        } else { //T <d1> <d2> <d3> <d4> <d5>
+                            //vetor com os dados a serem rolados
+
+                            boolean[] quais = new boolean[5];
+
+                            //preencho um vetor com os dados a serem rolados
+                            for(int i = 1; i < 6; i++){
+                                    quais[i-1] = args[i].equals("1");
+                            }   //rolo os dados
+                            rd.rolar(quais);
+                            response = rd.getStringDados();
+                            serverOut.println(response);
+                        }
+
+                        } while(serverIn.hasNextLine());
+                    }catch(Exception e){}
 		}
 
-		public void endComm() {
+		private void endComm() {
 
-			//fecho a conexao com o cliente
-			if(!flag){
-				serverOut.println("Total Score: " + s.players.get(this.id).getScore());
-				serverOut.println("Play Count: " + s.players.get(this.id).getPC());
-				serverOut.println("Bye");
-			}
-			try {
-				skt.close();
-			} catch (IOException ex) {}
+                    //fecho a conexao com o cliente
+                    if(!flag){
+                            serverOut.println("Total Score: " + s.players.get(this.id).getScore());
+                            serverOut.println("Play Count: " + s.players.get(this.id).getPC());
+                            serverOut.println("Avg. Score: " + (s.players.get(this.id).getScore()/s.players.get(this.id).getPC()));
+                            serverOut.println("Bye");
+                    }
+                    try {
+                            skt.close();
+                    } catch (IOException ex) {}
+                        
 		}
 	}
 	
@@ -217,83 +214,72 @@ public class Server {
 	 * a entrada para fechar o servidor deve ser "exit"
 	 */
 	private class InputThread extends Thread{
-		//guarda o endereco do servidor para fecha-lo
-		Server s;
-		
-		InputThread(Server s){
-			this.s = s;
-		}
-		
-		@Override
-		public void run(){
-			
-			String msg = null;
-			//leio mensagens da stdin
-			Scanner input = new Scanner(System.in);
-			do{
-				//quando ler "exit" seto a flag para fechar o programa
-				if(input.nextLine().equals("exit")){
-					s.setFlag();
-					break;
-				} else if(input.nextLine().equals("clear"))
-					clear();
-			} while(true);
-			
-			if(input.hasNextLine()) input.nextLine();
+            //guarda o endereco do servidor para fecha-lo
+            Server s;
 
-			//fecho o ServerSocket(para interromper o ss.accept e sair de um loop infinito)
-			try {
-				s.ss.close();
-			} catch (IOException ex) {}
-		}
+            InputThread(Server s){
+                    this.s = s;
+            }
+
+            @Override
+            public void run(){
+
+                String msg = null;
+                //leio mensagens da stdin
+                Scanner input = new Scanner(System.in);
+                do{
+                    //quando ler "exit" seto a flag para fechar o programa
+                    if(input.nextLine().equals("exit")){
+                        s.setFlag();
+                        break;
+                    }
+                } while(true);
+
+                //fecho o ServerSocket(para interromper o ss.accept e sair de um loop infinito)
+                try {
+                    s.ss.close();
+                } catch (IOException ex) {}
+            }
 	}
 	
 	/**
 	 * classe que encapsula jogadores
 	 */
 	private class Player implements Comparable<Player>{
-		private String id;
-		private Long score;
-		private Long playCount;
-		
-		public Player(String id, long score){
-			this.id = id;
-			this.score = score;
-			this.playCount = 0l;
-		}
-		
-		public long getScore() { return this.score; }
-		public String getId() { return this.id; }
-		public long getPC() { return this.playCount; }
-		
-		public void addScore(long score) { this.score += score; }
-		public void setId(String id) { this.id = id; }
-		
-		public void incPC() { this.playCount++; }
+            private String id;
+            private Long score;
+            private Long playCount;
 
-		@Override
-		public int compareTo(Player other) {
-			if(this.id.equals(other.id)) return 0;
-			if(this.score > other.getScore()) return 1;
-			else return -1;
-		}
-		
-		
+            public Player(String id, long score){
+                    this.id = id;
+                    this.score = score;
+                    this.playCount = 0l;
+            }
+
+            public long getScore() { return this.score; }
+            public String getId() { return this.id; }
+            public long getPC() { return this.playCount; }
+
+            public void addScore(long score) { this.score += score; }
+            public void setId(String id) { this.id = id; }
+
+            public void incPC() { this.playCount++; }
+
+            @Override
+            public int compareTo(Player other) {
+                    if(this.id.equals(other.id)) return 0;
+                    if(this.score > other.getScore()) return 1;
+                    else return -1;
+            }
 	}
 
 	/**
 	 * Funcao main
 	 * @param args the command line arguments
+     * @throws java.lang.Exception
 	 */
 	public static void main(String[] args) throws Exception{
 		Server s = new Server();
-		s.startServer(80);
-	}
-
-	private static void clear(){
-		final String ANSI_CLS = "\u001b[2J";
-		final String ANSI_HOME = "\u001b[H";
-		System.out.print(ANSI_CLS + ANSI_HOME);
-		System.out.flush();
+		s.startServer(9669);
 	}
 }
